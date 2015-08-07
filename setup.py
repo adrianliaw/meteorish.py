94a4698e875d9fed5f0c4f232dd3015d0bdfe331
adrianliaw <adrianliaw2000@gmail.com>
2015-08-07 21:13:42 +0800

    add dependencies

--- setup.py
+++ setup.py
@@ -9,11 +9,16 @@ setup(
                 "Building Meteor microservices with Python.",
     author="Adrian Liaw",
     author_email="adrianliaw2000@gmail.com",
-    license="Apache License 2.0",
+    license="Apache 2",
     url="https://github.com/adrianliaw/python-ddp-server",
     packages=["ddpserver"],
+    install_requires=[
+        "aiohttp>=0.15.1",
+        "sockjs>=0.2",
+        "meteor-ejson>=1.0.0",
+        ],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Programming Language :: Python :: 3.4",
-        ]
+        ],
     )
